711a9af601092c84e42b032b49f5a35c204b7878
NSCFBC <fabriciojunior67@gmail.com>
2022-09-25 04:47:36 -0300

    inicio do projeto restful java web mvc

---
+++ src/main/java/com/NoticiarioRestFulWeb/NoticiarioRestFulWebApplication.java
@@ -0,0 +1,13 @@
+package com.NoticiarioRestFulWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoticiarioRestFulWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoticiarioRestFulWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/NoticiarioRestFulWeb/NoticiarioRestFulWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.NoticiarioRestFulWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoticiarioRestFulWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
