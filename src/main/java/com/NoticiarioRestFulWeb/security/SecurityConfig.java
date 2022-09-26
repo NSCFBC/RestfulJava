c516a59f4936e96d0a0a8d29a47df9ac213c46d5
NSCFBC <fabriciojunior67@gmail.com>
2022-09-25 21:15:53 -0300

    implementacao token jwt e security

--- src/main/java/com/NoticiarioRestFulWeb/security/SecurityConfig.java
+++ src/main/java/com/NoticiarioRestFulWeb/security/SecurityConfig.java
@@ -1,17 +1,59 @@
 package com.NoticiarioRestFulWeb.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import com.NoticiarioRestFulWeb.jwt.JWTAuthenticationFilter;
+import com.NoticiarioRestFulWeb.jwt.JWTAuthorizationFilter;
+import com.NoticiarioRestFulWeb.service.UsuarioService;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-	@Override
-	public void configure(WebSecurity web) throws Exception {
-		web.ignoring().antMatchers("/**");
-	}
+    private UsuarioService userDetailsService;
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public SecurityConfig(UsuarioService userDetailsService, BCryptPasswordEncoder bCryptPasswordEncoder) {
+        this.userDetailsService = userDetailsService;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.cors().and().authorizeRequests()
+                .antMatchers(HttpMethod.POST, SecurityConstants.SIGN_UP_URL).permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .addFilter(new JWTAuthenticationFilter(authenticationManager()))
+                .addFilter(new JWTAuthorizationFilter(authenticationManager()))
+                // this disables session creation on Spring Security
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    }
+
+    @Override
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder);
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+
+        CorsConfiguration corsConfiguration = new CorsConfiguration().applyPermitDefaultValues();
+        source.registerCorsConfiguration("/**", corsConfiguration);
+
+        return source;
+    }
 }
 
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,11 +1,10 @@
-INSERT INTO usuario(id, email, nome, senha) VALUES (1, 'admin@gmail.com', 'bruna', 'adminAdmin');
 INSERT INTO usuario(id, email, nome, senha) VALUES (2, 'generator@generator', 'joao', 'joaoadin');
 INSERT INTO usuario(id, email, nome, senha) VALUES (3, 'Luana@Gmail.com', 'Luana', 'ajhdsakhanjksa');
 INSERT INTO usuario(id, email, nome, senha) VALUES (4, 'Maria@gmail.com', 'Maria', 'ddsnuisfdm');
 INSERT INTO usuario(id, email, nome, senha) VALUES (5, 'Fabricio@gmail.com', 'Fabricio', '1233213');
 INSERT INTO usuario(id, email, nome, senha) VALUES (6, 'Luiza@gmail.com', 'Luiza', 'da231das');
 INSERT INTO usuario(id, email, nome, senha) VALUES (7, 'Marcos@gmail.com', 'Marcos', '232dasew');
-INSERT INTO usuario(id, email, nome, senha) VALUES (8, 'admin@gmail.com', 'admin', '$2a$10$Qvt9h4nSifz9Tu8N.wiR.OHUrdU3BXXVoMWCdETKTQQK0S6LZMrJOW');
+INSERT INTO usuario(id, email, nome, senha) VALUES (8, 'admin@gmail.com', 'admin', '$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG');
 
 INSERT INTO role(id, role) VALUES (1,'ROLE_OPERATOR');
 INSERT INTO role(id, role)VALUES (2,'ROLE_ADMIN');
